c81aaf6d3c163b15e170c85aa4cb629bae36c683
George Skoufias <gskoufias@cognity.gr>
2019-09-03 18:36:36 +0300

    Finalized comments for factory pattern

--- factory-pattern/src/org/factorypat/Main.java
+++ factory-pattern/src/org/factorypat/Main.java
@@ -7,7 +7,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-
         /**
          *
          * This module involves building different products using the factory design pattern.
